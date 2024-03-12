d8d6f1727403299a09572700bc58cde197a444bb
copycatmay <maythril@gmail.com>
2024-03-12 16:07:32 -0400

    update
    day2

--- int1/js/script.js
+++ int1/js/script.js
@@ -1,4 +1,18 @@
-console.log("lets see if this remains in your code")
-console.log("it will")
+console.log("lets see if this remains in your code");
+console.log("it will");
 
+// increase letterspacing based on scroll distance
+$(window).scroll(function () {
+  let scroll = $(window).scrollLeft();
+  let letterSpacing = scroll / 50;
 
+  console.log(scroll);
+  $("#mainTitle h1").css("letter-spacing", letterSpacing + "px");
+  // $("#subTitle").css("letter-spacing", letterSpacing/4 + "px");
+
+  let newMargin = scroll; // Example calculation, adjust as needed
+  $(".akL").css("margin-left", newMargin * 0.0024 + "vw");
+  $(".tkL").css("margin-left", newMargin * 0.002 + "vw");
+  $(".erL").css("margin-left", newMargin * 0.0025 + "vw");
+  $(".saL").css("margin-left", newMargin * 0.001 + "vw");
+});
